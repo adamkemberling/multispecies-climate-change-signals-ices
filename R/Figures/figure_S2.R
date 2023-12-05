40dc57cb19310cb0ef104b64796dd59bebf4c36f
Carly Lovas <clovas@gmri.org>
2023-12-05 16:03:09 -0500

    annotating figures

--- R/Figures/figure_S2.R
+++ R/Figures/figure_S2.R
@@ -24,20 +24,62 @@ library(ggrepel)
 world <- ne_states(returnclass = "sf")
 cols <- c("#7D7C7C", "#22444B", "#0E6686", "#028ABD", "#02D2FF")
 
-decadal_map <- ggplot(data = world)+
-  geom_sf()+
+redfish <- ggplot(data = world)+
+  geom_sf()+ 
   coord_sf(xlim=c(-76, -66), ylim=c(37.5,45)) +
-  geom_point(data = decadal_map, aes(x=avg_lon, y= avg_lat, color = as.factor(Decade)), size = 1) +
+  geom_point(data = (decadal_map %>% filter(comname == "Acadian redfish")), aes(x=avg_lon, y= avg_lat, color = as.factor(Decade)), size = 1) +
   scale_color_manual(values = cols)+
-  facet_wrap(~factor(comname, levels = c("Acadian redfish", "Summer flounder", "American lobster")), nrow = 1) +
+  facet_wrap(~factor(comname, levels = c("Acadian redfish")), nrow = 1) +
   scale_y_continuous(breaks = c(36,40,44)) + scale_x_continuous(breaks = c(-78,-72,-66)) +
-  theme_gmri(legend.title = element_text(face = "bold", size = 8),
+  theme_gmri(legend.position = "none",
+             legend.title = element_text(face = "bold", size = 8),
              legend.text = element_text(size = 6), 
              axis.title = element_blank(),
              axis.text = element_text(size = 8), 
              strip.text = element_text(size = 8),
-             strip.background = element_rect(fill = "#00608A"), 
+             plot.margin = margin(0.5, 1.5, 0.5, 0),
+             strip.background = element_rect(fill = "#00608A", color = "#00608A"), 
              panel.border = element_rect(fill = NA, linetype = 1, linewidth = 0.2, color = "lightgray")) +
   guides(color = guide_legend(title = "Decade", size = 2, override.aes = list(size = 3)))
 
-ggsave(here("Figures", "Figure_S2.pdf"), decadal_map, width = 170, height = 79, units = "mm")
+flounder <- ggplot(data = world)+
+  geom_sf()+ 
+  coord_sf(xlim=c(-76, -66), ylim=c(37.5,45)) +
+  geom_point(data = (decadal_map %>% filter(comname == "Summer flounder")), aes(x=avg_lon, y= avg_lat, color = as.factor(Decade)), size = 1) +
+  scale_color_manual(values = cols)+
+  facet_wrap(~factor(comname, levels = c("Summer flounder")), nrow = 1) +
+  scale_y_continuous(breaks = c(36,40,44)) + scale_x_continuous(breaks = c(-78,-72,-66)) +
+  theme_gmri(legend.position = "bottom",
+             legend.title = element_text(face = "bold", size = 8),
+             legend.text = element_text(size = 6), 
+             axis.title = element_blank(),
+             axis.text = element_text(size = 8), 
+             strip.text = element_text(size = 8),
+             strip.background = element_rect(fill = "#00608A", color = "#00608A"), 
+             plot.margin = margin(0.5, 1.5, 0.5, 0),
+             panel.border = element_rect(fill = NA, linetype = 1, linewidth = 0.2, color = "lightgray")) +
+  guides(color = guide_legend(title = "Decade", size = 2, override.aes = list(size = 3)))
+
+lobster <- ggplot(data = world)+
+  geom_sf()+ 
+  coord_sf(xlim=c(-76, -66), ylim=c(37.5,45)) +
+  geom_point(data = (decadal_map %>% filter(comname == "American lobster")), aes(x=avg_lon, y= avg_lat, color = as.factor(Decade)), size = 1) +
+  scale_color_manual(values = cols)+
+  facet_wrap(~factor(comname, levels = c("American lobster")), nrow = 1) +
+  scale_y_continuous(breaks = c(36,40,44)) + scale_x_continuous(breaks = c(-78,-72,-66)) +
+  theme_gmri(legend.position = "none",
+             legend.title = element_text(face = "bold", size = 8),
+             legend.text = element_text(size = 6), 
+             axis.title = element_blank(),
+             axis.text = element_text(size = 8), 
+             strip.text = element_text(size = 8),
+             strip.background = element_rect(fill = "#00608A", color = "#00608A"), 
+             plot.margin = margin(0.5, 1.5, 0.5, 0),
+             panel.border = element_rect(fill = NA, linetype = 1, linewidth = 0.2, color = "lightgray")) +
+  guides(color = guide_legend(title = "Decade", size = 2, override.aes = list(size = 3)))
+
+fig_s2 <- wrap_plots(redfish, flounder, lobster) + 
+  plot_annotation(theme      = theme(plot.title = element_text(size = 5)), 
+                  tag_levels = list(c("A.", "B.", "C.")))
+
+ggsave(here("Figures", "Figure_S2.pdf"), fig_s2, width = 170, height = 80, units = "mm")
